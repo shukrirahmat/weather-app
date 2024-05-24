b0df1077c4b51578dd193f5d443bca434e8d7268
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-05-24 14:40:14 +0800

    can now return data with console log

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import "./styles.css";
 
-console.log("ready");
+const API_KEY = "4008195c51d44444ac4134527242005";
+
+const getWeather = async function getWeather(location) {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`,
+    {
+      mode: "cors",
+    }
+  );
+  const data = await response.json();
+
+  const locationName = data.location.name;
+  const country = data.location.country;
+  const condition = data.current.condition.text;
+  const temp_c = data.current.temp_c;
+  const temp_f = data.current.temp_f;
+
+  console.log([locationName, country, condition, temp_c, temp_f]);
+};
+
+getWeather("kuala lumpur");
