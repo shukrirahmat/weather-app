8a59dcf8d888e3096697fe59f0af013b93e82c16
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-05-27 00:42:13 +0800

    fix fetch not secure

--- src/getWeather.js
+++ src/getWeather.js
@@ -5,7 +5,7 @@ const API_KEY = "4008195c51d44444ac4134527242005";
 const getWeather = async function getWeather(location) {
   try {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=3&aqi=no&alerts=no`,
+      `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=3&aqi=no&alerts=no`,
       {
         mode: "cors",
       }
@@ -16,7 +16,7 @@ const getWeather = async function getWeather(location) {
     displayWeatherResult(data);
 
   } catch (error) {
-    
+
     console.log(error);
     displayError();
   }
